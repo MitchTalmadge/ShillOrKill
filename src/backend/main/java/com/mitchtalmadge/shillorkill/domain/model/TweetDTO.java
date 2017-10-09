4e410c3edddfcc87fd317f5221712407c06a50b4
Mitch Talmadge <mitch@mitchtalmadge.com>
2017-10-08 20:48:00 -0600

    Added TweetDTO

---
+++ src/backend/main/java/com/mitchtalmadge/shillorkill/domain/model/TweetDTO.java
@@ -0,0 +1,19 @@
+package com.mitchtalmadge.shillorkill.domain.model;
+
+import java.util.Date;
+
+public class TweetDTO {
+
+    public long id;
+
+    public long statusId;
+
+    public Date createdAt;
+
+    public String text;
+
+    public String authorName;
+
+    public String authorImageUrl;
+
+}
